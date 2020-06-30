d8ec81a4bc834a52d1daaa586eaee20bc8a60253
agur.xu <agur.xu@tokentuber.com>
2020-06-30 08:51:48 +0800

    add antd

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+exports.onCreateBabelConfig = ({ actions }, { style }) => {
+    actions.setBabelPlugin({
+      name: `babel-plugin-import`,
+      options: {
+        libraryName: "antd",
+        libraryDirectory: "es",
+        style: style === true ? style : "css"
+      }
+    });
+  };
--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+const SkillHeader = ({ siteTitle }) => (
   <header
     style={{
       background: `rebeccapurple`,
@@ -31,12 +31,12 @@ const Header = ({ siteTitle }) => (
   </header>
 )
 
-Header.propTypes = {
+SkillHeader.propTypes = {
   siteTitle: PropTypes.string,
 }
 
-Header.defaultProps = {
+SkillHeader.defaultProps = {
   siteTitle: ``,
 }
 
-export default Header
+export default SkillHeader
