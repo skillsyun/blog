463b0407efcbdbb829a951921d2522e408f7f876
agur.xu <agur.xu@tokentuber.com>
2020-06-30 21:55:33 +0800

    1. update Dependencies 2. use gatsby-starter-lumen

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,4 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+'use strict';
 
-// You can delete this file if you're not using it
-exports.onCreateBabelConfig = ({ actions }, { style }) => {
-    actions.setBabelPlugin({
-      name: `babel-plugin-import`,
-      options: {
-        libraryName: "antd",
-        libraryDirectory: "es",
-        style: style === true ? style : "css"
-      }
-    });
-  };
+exports.createPages = require('./gatsby/create-pages');
+exports.onCreateNode = require('./gatsby/on-create-node');
